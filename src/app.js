e17fa0f097876e0c95d2b3d5f3cde8318f6dd1e0
sirajaku <sirajaku@cisco.com>
2021-01-23 22:15:19 +0530

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
